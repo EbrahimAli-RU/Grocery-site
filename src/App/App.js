8f3c7b03b50a71d903d43a2c8b830b7270787714
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2020-12-19 21:18:39 +0600

    Installed bootstrap and material-ui now will run eject

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'reactstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <Button color="primary" outline>Check</Button>
+    </div>
+  );
+}
+
+export default App;
